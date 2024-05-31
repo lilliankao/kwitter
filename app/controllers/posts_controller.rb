1c0e2c8b13fbbe0460ebbf0ddc1c63e7798e257e
Lillian Kao <lillian.kao@kellogg.northwestern.edu>
2024-05-31 20:16:00 +0000

    fix bug publish api

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,21 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.order("id desc")
+    posts_json = []
+    for post in posts
+      user = User.find_by({"id" => post["user_id"]}) 
+      posts_json <<{
+        "username" => user["username"] 
+        "real_name" => user["real_name"]
+        "body" => post["body"],
+        "created_at" => post "created_at"
+      }
+    end
+
+    respond_to do |format|
+      format.html
+      format.json {render :json => @posts_json}
+
   end
 
   def create
